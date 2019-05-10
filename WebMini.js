711a24d82d6da663476f618016f3ac279ad0583f
Bakinak <akobbe16@student.aau.dk>
2019-05-10 14:10:29 +0200

    Great buttons at bottom lull. Bottom button buttom button bottom button

--- WebMini.js
+++ WebMini.js
@@ -1,18 +1,20 @@
 
 //This is quite possibly the worst function ever created, but alas, here we are.
-$('nav a').on('click', function(e) {                 // User clicks nav link
+$('ul a').on('click', function(e) {                 // User clicks nav link
   e.preventDefault();                                // Stop loading new link
   var url = this.href;                               // Get value of href
-  var target = '#'+$(this).attr("class").split(" ")[1];
   var newcontent = '#'+$(this).attr("class").split(" ")[0];
+  var target = '#'+$(this).attr("class").split(" ")[1];
   var replace = '#'+$(this).attr("class").split(" ")[2];
 
 
-  $('nav a.current').removeClass('current');         // Clear current indicator
+  $('ul a.current').removeClass('current');         // Clear current indicator
   $(this).addClass('current');                       // New current indicator
   $(replace).remove();                              // Remove old content
   //ID of old content to remove
   $(target).load(url +' '+ newcontent).hide().fadeIn('slow'); // New content
   //Where to place it      //The new text
-  alert(replace);
+
+  //alert(replace);
+  //alert lets you display a message, good for testing whether variables have the right values
 });
