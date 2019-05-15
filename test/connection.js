e691b3ad3fea3028affabd10a068704b8497375a
finjaxsonAAU <mnguye15@student.aau.dk>
2019-05-15 17:12:54 +0200

    well shit here we go again

---
+++ test/connection.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+
+// ES6 Promises
+mongoose.Promise = global.Promise;
+
+//connect to the db before test run
+before(function(done){
+  //connect to mongodb
+mongoose.connect('mongodb://localhost/data');
+mongoose.connection.once('open',function(){
+  console.log('Connection has been made, now make fireworks...');
+  done();
+}).on('error',function(error){
+  console.log('connection error', error);
+  });
+});
+
+
+//drop the charactors collection before each test
+beforeEach(function(done){
+  mongoose.connection.collections.mariochars.drop(function(){
+    done();
+  });
+});
