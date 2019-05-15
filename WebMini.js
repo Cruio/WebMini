25ae98b01f119a8c151beeac01c185fd76413871
Bakinak <akobbe16@student.aau.dk>
2019-05-15 19:21:01 +0200

    something about buttons and score update

--- WebMini.js
+++ WebMini.js
@@ -1,6 +1,6 @@
 
 //This is quite possibly the worst function ever created, but alas, here we are.
-$('ul a').on('click', function(e) {                 // User clicks nav link
+$('#nav a').on('click', function(e) {                 // User clicks nav link
   e.preventDefault();                                // Stop loading new link
   var url = this.href;                               // Get value of href
   var newcontent = '#'+$(this).attr("class").split(" ")[0];
@@ -8,13 +8,26 @@ $('ul a').on('click', function(e) {                 // User clicks nav link
   var replace = '#'+$(this).attr("class").split(" ")[2];
 
 
-  $('ul a.current').removeClass('current');         // Clear current indicator
-  $(this).addClass('current');                       // New current indicator
   $(replace).remove();                              // Remove old content
   //ID of old content to remove
   $(target).load(url +' '+ newcontent).hide().fadeIn('slow'); // New content
   //Where to place it      //The new content
 
-  //alert(replace);
+  //alert(url);
   //alert lets you display a message, good for testing whether variables have the right values
 });
+
+$('#scoreRefresh a').on('click', function(e){
+  e.preventDefault();
+  var url = this.href;
+  $('#listOfScores').remove();                              // Remove old content
+  //ID of old content to remove
+  $('#scoreContainer').load(url + ' #mongoScore').hide().fadeIn('slow');
+    //Where to place it      //The new content
+});
+
+
+function hello(){
+  var blargh ="hello there young mango"
+  document.getElementById('content').innerHTML = blargh;
+}
